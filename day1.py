3cee127350f9d869df909c79b73346d572e4bb49
KATARZYNA ZYSKO <zysko.kz@pg.com>
2022-12-01 09:07:05 +0100

    Day 1 finished

--- day1.py
+++ day1.py
@@ -1,11 +1,32 @@
 from aocd.models import Puzzle
+
 """
 Remember to add AOC_SESSION as an environment variable. It is a cookie from AOC website called session.
 """
 
-
 def get_puzzle():
     puzzle = Puzzle(year=2022, day=1)
     data = puzzle.input_data
-    data_split = [line for line in data.split("\n")]
-    return data_split
+    data = [line.split('\n') for line in data.split("\n\n")]
+    data = [[int(x) for x in data] for data in data]
+    data = [sum(x) for x in data]
+    return data
+
+
+def part_1():
+    data = get_puzzle()
+    return max(data)
+
+
+def part_2():
+    data = get_puzzle()
+    calories = 0
+    for i in range(3):
+        calories += max(data)
+        data.remove(max(data))
+
+    return calories
+
+
+print(f"Answer for part 1 is {part_1()}")
+print(f"Answer for part 2 is {part_2()}")
